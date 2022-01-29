d00846b5069854c1563f5ff30cd52d54372b0765
Joel Bennett <Jaykul@HuddledMasses.org>
2022-01-29 17:40:56 -0500

    Turn down gitversion verbosity to fix #1

--- gitversion/action.ps1
+++ gitversion/action.ps1
@@ -10,7 +10,7 @@ dotnet tool install --global GitVersion.Tool # --version 5.6.0
 Write-Host "::endgroup::"
 
 $ofs = "`n"
-[string]$gitversion = dotnet-gitversion -url "https://github.com/$repository.git" -b $ref -c $sha
+[string]$gitversion = dotnet-gitversion -url "https://github.com/$repository.git" -b $ref -c $sha -verbosity quiet
 Write-Verbose $gitversion -Verbose
 
 (ConvertFrom-Json -InputObject $gitversion -AsHashtable).GetEnumerator().ForEach{
